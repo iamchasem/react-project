7d2ac47dd75677a70d8ab8e14634afcb5bafc9c8
Chase McClain <iamchasem.dev@gmail.com>
2023-04-23 14:16:03 -0700

    completed intro exercises

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Learning React is Fun So Far!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>React is so Cool! And makes JS more fun!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +19,17 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          <li>I</li>
+          <li>Like</li>
+          <li>React</li>
+          <li>A</li>
+          <li>Lot</li>
+          <li>!!!</li>
+        </ul>
       </header>
     </div>
-  );
+  ); 
 }
 
 export default App;
